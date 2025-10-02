b20aaf500f39c508b4697e63e48cfefa3a830022
Deimer <deimer.ospino.arteta@gmail.com>
2025-10-01 21:43:47 -0500

    imagen de logo cambiado

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,19 @@
-import { Leaf } from 'lucide-react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-2" aria-label="Natural healt center">
-      <Leaf className="h-8 w-8 text-primary" />
+    <Link 
+      href="/" 
+      className="flex items-center gap-2" 
+      aria-label="Natural health center"
+    >
+      <Image 
+        src="/logo.png"  // Next busca en /public
+        alt="Logo Natural Health Center"
+        width={40}       // ajusta el tamaño
+        height={40}
+      />
       <span className="font-headline text-2xl font-bold text-primary">
         Natural heal center
       </span>
